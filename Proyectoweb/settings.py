563f90c936cf0b7239a57b85f7219817141f6564
Vicferron <65445340+Vicferron@users.noreply.github.com>
2020-08-26 21:11:22 +0200

    Update settings.py

--- Proyectoweb/settings.py
+++ Proyectoweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0t5covc2%-0*b$=0p(o+dghm4+8m-@bl=ziu_v6n%xj*8mjd7m'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
